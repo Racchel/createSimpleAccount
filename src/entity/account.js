48934c8755781da2a32e1b45b2b8493a88bae6df
Racchel <racchelvelasco@gmail.com>
2022-02-23 16:21:23 -0300

    password validation - working

--- src/entity/account.js
+++ src/entity/account.js
@@ -71,10 +71,16 @@ export default class Account {
   }
 
   checkPassword (password) {
-    // const regex = /^(?=.*[A-Z])(?=.*[!#@$%&])(?=.*[0-9])(?=.*[a-z]).{8, 20}$/
+    /*
+      (?=.*\d)              // deve conter ao menos um dígito
+      (?=.*[a-z])           // deve conter ao menos uma letra minúscula
+      (?=.*[A-Z])           // deve conter ao menos uma letra maiúscula
+      (?=.*[$*&@#])         // deve conter ao menos um caractere especial
+      [0-9a-zA-Z$*&@#]{8,}  // deve conter ao menos 8 dos caracteres mencionados
+    */
 
-    // return regex.test(password)
-    // ver um regex que funfa
-    return true
+    const regex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#])[0-9a-zA-Z$*&@#]{8,}$/
+
+    return regex.test(password)
   }
 }
